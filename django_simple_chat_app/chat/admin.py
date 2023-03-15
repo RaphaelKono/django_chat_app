79333e992d84d658c1885fa4d59c37961cfbd901
RaphaelKono <github-20230129-1818@konopatzki.info>
2023-03-15 18:07:39 +0100

    input into backend

--- django_simple_chat_app/chat/admin.py
+++ django_simple_chat_app/chat/admin.py
@@ -1,11 +1,15 @@
 from django.contrib import admin
 from .models import Message
+from .models import Chat
 
 class MessageAdmin(admin.ModelAdmin):
-    fields = ('text','created_at','author','receiver')
+    fields = ('chat','text','created_at','author','receiver')
     list_display = ('text','created_at','author','receiver')
     search_fields = ('text',)
 
+class ChatAdmin(admin.ModelAdmin):
+    pass
 
 # Register your models here.
 admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat, ChatAdmin)
--- django_simple_chat_app/chat/views.py
+++ django_simple_chat_app/chat/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
+from .models import Message
+from .models import Chat
 
 # Create your views here.
 def index(request):
+    if request.method == 'POST':
+        print('Received message.' + request.POST['txtmsg'])
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(text = request.POST['txtmsg'], chat=myChat, author=request.user, receiver=request.user)
     return render(request, 'chat/index.html', {'username':'Raphael'})
